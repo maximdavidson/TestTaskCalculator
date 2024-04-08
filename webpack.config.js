289f0acd8dfb4d82b29a21bd77bf559dd83a8ae1
Maxim Davidson <maxd02122004@gmail.com>
2024-04-08 23:24:06 +0300

    Final code first

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,20 @@
-// const path = require('path');
+const path = require('path');
 
-
-// module.exports = {
-//   entry: './src/index.js', // Входной файл
-//   output: {
-//     filename: 'main.js', // Выходной файл
-//     path: path.resolve(__dirname, 'dist'), // Папка для выходного файла
-//   },
-//   mode: 'development', // Режим сборки
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env', '@babel/preset-react']
-//           }
-//         }
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: ['.js', '.jsx'], // Указывает расширения файлов, которые будет обрабатывать webpack
-//     alias: {
-//       '@components': path.resolve(__dirname, './src/components'),
-//     }
-//   }
-// };
-
-
-  // "scripts": {
-  //   "start": "react-app-rewired start",
-  //   "build": "react-app-rewired build",
-  //   "test": "react-app-rewired test",
-  //   "eject": "react-scripts eject"
-  // },
+module.exports = {
+  entry: './src/index.js', // точка входа вашего приложения
+  output: {
+    filename: 'mainweb.js', // имя выходного файла
+    path: path.resolve(__dirname, 'dist'), // путь к каталогу выходных файлов
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // регулярное выражение, которое ищет все файлы .js
+        exclude: /node_modules/, // исключает все файлы в каталоге node_modules
+        use: {
+          loader: 'babel-loader', // использует babel-loader для транспиляции вашего кода
+        },
+      },
+    ],
+  },
+};
